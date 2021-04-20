b2aac58df8a64579a577f70d96305623d412c033
regevazran <regevazran@gmail.com>
2021-04-20 14:56:46 +0300

    first commit

--- excel_to_data_set.py
+++ excel_to_data_set.py
@@ -1,5 +1,46 @@
 from defines import *
 
-def creat_data_set(data_set_fieldes_list):
+class Mouse:
+    def __init__(self):
+        self.name = None
+        self.contraction = None
+        self.scab = None
+        self.wound_close = None
+        self.wound_size_by_pixel = None
+        self.wound_size_by_cm = None
+        self.pictures = None
 
-    return
+    def add_name(self,mouse_name, exp_name):
+        self.name = str(exp_name) + "_" + str(mouse_name)
+
+    def add_day(self,day,contraction,scab,wound_close,wound_size_by_pixel,wound_size_by_cm,pictures):
+        self.contraction[day] = contraction
+        self.scab[day] = scab
+        self.wound_close[day] = wound_close
+        self.wound_size_by_pixel[day] = wound_size_by_pixel
+        self.wound_size_by_cm[day] = wound_size_by_cm
+        self.pictures[day] = pictures
+
+
+
+class DataSetGenerator:
+    def __init__(self, path, exp_name, dataset=None):
+        self.excel_path = path
+        self.dataset = dataset if dataset is not None else pd.DataFrame(columns=['Mouse','day0','day1','day2','day3','day4','day5','day6','day7','day8','day9','day10'])
+        self.exp_name = exp_name
+
+
+    def get_mice_name_list(self):
+        pass
+
+    def create_new_mouse(self,mouse_name):
+        mouse = Mouse()
+        mouse.add_name(mouse_name=mouse_name,exp_name=self.exp_name)
+
+    def update_mice_features(self):
+        pass
+    def add_mice_to_dataset(self):
+        pass
+    def add_to_data_set(self,mouse):
+        pass
+
--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from defines import *
+from excel_to_data_set import DataSetGenerator
+
+
+def main():
+
+
+    df = pd.ExcelFile(r'/Users/regevazran/Desktop/technion/semester i/project c/AWHA1 features.xls')
+    file = open(df,'r')
+    lines = file.readlines()
+    for line in lines :
+        print(line)
+
+    return
+    data_set = None
+    excel_path = [r'/Users/regevazran/Desktop/technion/semester i/project c/AWHA1 features.xls']
+    data_generator = DataSetGenerator(path=excel_path, dataset=data_set)
+    data_generator.get_mice_name_list()
+    data_generator.update_mice_features()
+    data_generator.add_mice_to_dataset()
 
 
 
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    df = pd.read_excel(r'/Users/regevazran/Desktop/technion/semester i/project c/AWHA1 features.xls')
-    print(df.to_string())
-    print(df["Mice"])
+    main()
