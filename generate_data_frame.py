48eeab1c2e22fc54af69b676dfb9af6897d5c479
Tomer Keren <tomerkeren42@gmail.com>
2021-04-25 22:33:40 +0300

    Added excel file path mechanism

--- generate_data_frame.py
+++ generate_data_frame.py
@@ -1,31 +1,32 @@
 import pandas as pd
-
+from tkinter import filedialog
+import os
 
 class DataSetGenerator:
-    def __init__(self, path, dataset=None):
-        self.excel_path = path
+    def __init__(self, path=None):
+        self.excel_path = path if path is not None else filedialog.askopenfilename(filetypes=[("Excel files", ".xlsx .xls .csv")])
         self.data_frame = None
         self.mice_names = []
         # self.dataset = dataset if dataset is not None else pd.DataFrame(columns=['Mouse','day0','day1','day2','day3','day4','day5','day6','day7','day8','day9','day10'])
         # self.exp_name = None
 
     def initialize_data_frame(self):
-        df = pd.read_excel(self.excel_path, engine='openpyxl', index_col=False)
-        self.data_frame = df.dropna(axis='columns')
+        try:
+            df = pd.read_excel(self.excel_path, engine='openpyxl', index_col=False)
+            self.data_frame = df.dropna(axis='columns')
+        except FileNotFoundError as file_not_found_msg:
+            print(file_not_found_msg, "\nPlease try to select the file manually\n")
+            self.excel_path = filedialog.askopenfilename(filetypes=[("Excel files", ".xlsx .xls .csv")])
+            self.initialize_data_frame()
 
     def get_mice_name_list(self):
-        for name in self.data_frame.loc[:, "Mice"]:
-            if name not in self.mice_names:
-                self.mice_names.append(name)
+        try:
+            for name in self.data_frame.loc[:, "Mice"]:
+                if name not in self.mice_names:
+                    self.mice_names.append(name)
+        except KeyError as key_error_msg:
+            print(key_error_msg)
+            exit(-1)
 
-    # def create_new_mouse(self,mouse_name):
-    #     mouse = Mouse()
-    #     mouse.add_name(mouse_name=mouse_name,exp_name=self.exp_name)
 
-    def update_mice_features(self):
-        pass
-    def add_mice_to_dataset(self):
-        pass
-    def add_to_data_set(self,mouse):
-        pass
 
--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 from generate_data_frame import DataSetGenerator
 
-
+# TODO: Regev, try to give datasetgenerator wrong path and see what happens.
+# TODO: also, try to give it the right path. and try not give path at all. IT ALL WORKS! :)
 def main():
-
-    excel_path = "C:/Users/tomer/OneDrive/שולחן העבודה/try_for_mouse.xlsx"
-    data_set = None
-    data_generator = DataSetGenerator(path=excel_path, dataset=data_set)
+    data_generator = DataSetGenerator()
     data_generator.initialize_data_frame()
     data_generator.get_mice_name_list()
 
+    print(f"Hope to make you proud!!\nData frame is:\n{data_generator.data_frame}\nMice names are:\n{data_generator.mice_names}")
     # data_generator.update_mice_features()
     # data_generator.add_mice_to_dataset()
-
-
-
-
 
 if __name__ == '__main__':
     main()
