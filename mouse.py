056ad6398b3d70916fb9287762946eae92b9fb54
Tomer Keren <tomerkeren42@gmail.com>
2021-06-08 14:40:55 +0300

    tryyyy

--- mouse.py
+++ mouse.py
@@ -1,9 +1,7 @@
-
-
 class Mouse:
     def __init__(self):
-        self.name = None
-        self.contraction = None
+        self.name = ""
+        self.contraction = False
         self.scab = None
         self.wound_close = None
         self.wound_size_by_pixel = None
@@ -13,11 +11,10 @@ class Mouse:
     def add_name(self, mouse_name, exp_name):
         self.name = str(exp_name) + "_" + str(mouse_name)
 
-    def set_data(self, contraction, scab = None, wound_close = None, wound_size_by_pixel = None, wound_size_by_cm = None, pictures = None):
+    def set_data(self, contraction, scab=None, wound_close=None, wound_size_by_pixel=None, wound_size_by_cm=None, pictures=None):
         self.contraction = contraction
         self.scab = scab
         self.wound_close = wound_close
         self.wound_size_by_pixel = wound_size_by_pixel
         self.pictures = pictures
         self.wound_size_by_cm = wound_size_by_cm
-
